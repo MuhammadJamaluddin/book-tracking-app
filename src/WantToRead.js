104efa89a454564f9393a8ab0f84506e6bf0eeb4
Jamal <mohamed.eldeen.399@gmail.com>
2021-03-01 00:18:16 +0200

    chore cleanup console statements

--- src/WantToRead.js
+++ src/WantToRead.js
@@ -1,21 +1,17 @@
 import Book from './Book';
 import { filterBooksByShelf } from './CurrentlyReading';
 
-const WantToRead = ({ books, setBooks }) => {
-  console.log('books here', books);
-
-  return (
-    <div className="bookshelf">
-      <h2 className="bookshelf-title">Want To Read</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          {filterBooksByShelf(books, 'wantToRead')?.map((book) => (
-            <Book key={book.id} book={book} books={books} setBooks={setBooks} />
-          ))}
-        </ol>
-      </div>
+const WantToRead = ({ books, setBooks }) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">Want To Read</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {filterBooksByShelf(books, 'wantToRead')?.map((book) => (
+          <Book key={book.id} book={book} books={books} setBooks={setBooks} />
+        ))}
+      </ol>
     </div>
-  );
-};
+  </div>
+);
 
 export default WantToRead;
